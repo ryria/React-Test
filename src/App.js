e8ecca8c0ce3c48377aafc6ff9b95d7529c35250
Robert <rmj.coulter1@gmail.com>
2022-02-07 21:19:15 +1100

    3

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ class App extends Component
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <Squares />
+          <Grid />
         </header>
       </div>
     )
