8de03bfdd49e046513c4e97a4a7459b400572a68
ryria <97291101+ryria@users.noreply.github.com>
2022-02-07 20:58:43 +1100

    x

--- src/App.js
+++ src/App.js
@@ -2,6 +2,50 @@ import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 
+
+//create textbox component that when clicked cycles color between Grey > Green > Yello
+class TextBox extends Component {
+  state = {
+    color: "grey"
+  }
+  changeColor = () => {
+    this.setState({
+      color: "green"
+    })
+  }
+  render() {
+    return (
+      <div
+        style={{
+          width: "100px",
+          height: "100px",
+          backgroundColor: this.state.color
+        }}
+        onClick={this.changeColor}
+      />
+    )
+  }
+}
+
+//create 6x6 grid of TextBox components
+class Grid extends Component {
+  render() {
+    let rows = []
+    for (let i = 0; i < 6; i++) {
+      let cols = []
+      for (let j = 0; j < 6; j++) {
+        cols.push(<TextBox />)
+      }
+      rows.push(<div className="row">{cols}</div>)
+    }
+    return <div className="grid">{rows}</div>
+  }
+}
+
+
+
+
+
 class LambdaDemo extends Component {
   constructor(props) {
     super(props)
